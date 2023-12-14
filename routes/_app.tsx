447a21ecee474a13ad187da5cdd7d6765f4f6346
ionoh <y0a6s5u8soy@gmail.com>
2023-12-14 23:48:27 +0900

    fix typo

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>ngs-equipment-caluclator</title>
+        <title>NGS Equipment Calculator</title>
         <link rel="stylesheet" href="/styles.css" />
         <script src="https://cdn.tailwindcss.com" async defer></script>
       </head>
